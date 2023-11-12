ba2f2e6ac5fa40d7c880cbb225249262cc3815aa
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-12 20:25:08 +0900

    add bullet

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,12 +3,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
 
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; // half of the width
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; // half of the height
+    }
+
+    // Update is called once per frame
     void Update()
     {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+
+        // Movement code here...
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
         transform.position += movement;
     }
---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.up * bulletSpeed;
+    }
+}
