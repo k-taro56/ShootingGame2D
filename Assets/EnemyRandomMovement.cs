ec9d851d298c06fa77c16056795bdea4c2774b33
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-14 19:37:43 +0900

    enemy chase player

--- Assets/EnemyRandomMovement.cs
+++ Assets/EnemyRandomMovement.cs
@@ -8,25 +8,22 @@ public class EnemyRandomMovement : MonoBehaviour
     private Vector2 moveDirection;
     private float timer;
 
+    private GameObject target;
+
     void Start()
     {
-        MoveDirection();
+        target = GameObject.FindGameObjectWithTag("Player");
+        ChasePlayer();
     }
 
     void Update()
     {
-        transform.Translate(moveDirection * speed * Time.deltaTime);
-
-        timer += Time.deltaTime;
-        if (timer > changeDirectionTime)
-        {
-            MoveDirection();
-            timer = 0f;
-        }
+        ChasePlayer();
     }
 
-    private void MoveDirection()
+    void ChasePlayer()
     {
-        moveDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 }
