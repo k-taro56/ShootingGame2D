1ccd80df2b7f7cf6921dc377065d4c2b377a638d
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-15 02:17:18 +0900

    Operational up

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -15,9 +15,8 @@ public class PlayerShooting : MonoBehaviour
         }
     }
 
-    void Shoot()
+    public void Shoot()
     {
-
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.velocity = firePoint.up * bulletSpeed;
